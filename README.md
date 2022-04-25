c9b606d137e643e801dcabef2b951f1bbd8ecb87
toddbauer86 <98363909+toddbauer86@users.noreply.github.com>
2022-04-24 19:26:55 -0500

    working program

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Note Taker Starter Code
+# Note Taker
+
+## Description
+
+This application allows the user to enter notes and as they save, they appear in the sidebar on the left. Notes can be deleted by clicking the trash can next to the corresponding note.
+
+## Screenshot
+
+## Application Link
--- server.js
+++ server.js
@@ -1,18 +1,22 @@
-const express = require("express");
 const PORT = process.env.PORT || 3001;
-
+const express = require("express");
 const app = express();
+const fs = require("fs");
+const path = require("path");
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
 
-const htmlRoutes = require("./routes/htmlroutes");
-const apiRoutes = require("./routes/apiroutes");
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.static("public"));
+app.use(express.json());
 app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use(express.static("public"));
-
-app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
-);
+app.listen(PORT, () => {
+  console.log(`Server is up on port ${PORT}!`);
+});
